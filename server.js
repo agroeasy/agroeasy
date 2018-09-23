e5dd5a8a35d1d39ac9c768d60fe1faab108521df
EngrEric <okemmadueric@gmail.com>
2018-09-23 21:33:42 +0100

    server.js update

--- server.js
+++ server.js
@@ -4,18 +4,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-//API calls
-app.get('/api',(req,res)=>{
-    res.send({express:"Express at work here"});
-});
+    app.use(express.static(path.join(__dirname, '/client/public')));
 
-if(process.env.NODE_ENV === 'production'){
-    //serve the static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-
-    //Handle react routing, return all requests to react app
     app.get('*', function(req, res){
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '/client/public', 'index.html'));
     });
-}
 app.listen(port, ()=>console.log(`Listening on port ${port}`));
