1ef75a9c6e0876f61b37868566773a9ed1d1432c
EngrEric <okemmadueric@gmail.com>
2018-10-24 17:04:00 +0100

    update

--- helpers/db.js
+++ helpers/db.js
@@ -1,9 +1,9 @@
 'use strict';
-const mongodbUrl = require('../config/config');
+const { connectionString } = require('../config/config');
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
-mongoose.connect(mongodbUrl.connectionString, { useNewUrlParser: true });
+mongoose.connect(connectionString, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 const db = mongoose.connection;
