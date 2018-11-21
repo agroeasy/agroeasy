54974ddbd9c009f83cda124dd560d3ce51be215a
Joshua Obinna <johuaobinna93@gmail.com>
2018-11-21 16:34:14 +0100

    changes

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -5,12 +5,12 @@ const baseConfig = require('./webpack.base.config');
 const optimizationConfig = require('./webpack.opt.config');
 
 const productionConfiguration = env => {
-  const NODE_ENV = env.NODE_ENV ? env.NODE_ENV : 'development';
-  return {
-    plugins: [
-      new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(NODE_ENV) }),
-    ],
-  };
+    const NODE_ENV = env.NODE_ENV ? env.NODE_ENV : 'development';
+    return {
+        plugins: [
+            new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(NODE_ENV) }),
+        ],
+    };
 };
 
 module.exports = merge.smart(baseConfig, optimizationConfig, productionConfiguration);
