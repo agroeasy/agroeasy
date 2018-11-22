44f1d64f607360e3c532e2061cc957a438edc72d
Joshua Obinna <johuaobinna93@gmail.com>
2018-11-22 17:25:44 +0100

    changes

--- src/components/rows/components/MoreInfo.js
+++ src/components/rows/components/MoreInfo.js
@@ -29,7 +29,7 @@ MoreInfo.PropTypes = {
     location: PropTypes.string,
     quantity: PropTypes.number,
     phone: PropTypes.string,
-    pix: PropTypes.Image,
+    pix: PropTypes.string,
     tag: PropTypes.string,
     title: PropTypes.string,
 
