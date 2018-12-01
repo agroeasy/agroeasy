a80cb9a9e998e0c6f3e6657bab74a694440a31f9
EngrEric <okemmadueric@gmail.com>
2018-12-01 16:24:56 +0100

    update

--- routes/producer/index.js
+++ routes/producer/index.js
@@ -9,15 +9,11 @@ const {
 } = require('./helper');
 
 router.get('/findAll', allProducersDetails); // find all
-
 // the end point to find producers saved in database using id
 router.get('/:id', producerDetails);
-
 // the end point to create products in the database
 router.post('/create', producerCreate);
-
 router.put('/:producersId', producerUpdate);// update by id
-
 router.delete('/:id', producerDelete);// delete by id
 
 module.exports = router;
--- routes/product/index.js
+++ routes/product/index.js
@@ -9,15 +9,11 @@ const {
 } = require('./helper');
 
 router.get('/findAll', allProductsDetails);
-
 // the end point to find products saved in database
 router.get('/:id', productDetails);
-
 // the end point to create product and save to database
 router.post('/create', productCreate);
-
 router.put('/:productsId', productUpdate);
-
 router.delete('/:id', productDelete);
 
 module.exports = router;
