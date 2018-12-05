5b85fa8ef4fff4afb5277af7b8c401c7d047f658
EngrEric <okemmadueric@gmail.com>
2018-12-05 21:19:16 +0100

    upate

--- db/config.js
+++ db/config.js
@@ -1,3 +1,10 @@
+/**
+ * Defines the url for the database.
+ *
+ * @constant
+ */
+const DATA_BASE_URL = 'mongodb://localhost/agroeasyDB';
+
 module.exports = {
-    MONGO_DB_URL: 'mongodb://localhost/agroeasyDB',
+    MONGO_DB_URL: DATA_BASE_URL,
 };
