0f53ff00437cb16c0c361fa9aaf8164a0b2bd9fd
engreric <okemmadueric@gmail.com>
2019-01-03 00:38:58 +0100

    add more comments

--- src/components/signin/actionTypes.js
+++ src/components/signin/actionTypes.js
@@ -1,3 +1,23 @@
-export const SIGNIN_REQUEST = "SIGN_REQUEST";
-export const SIGNIN_SUCCESS = "SIGNIN_SUCCESS";
+/**
+ * Fired by the {@link actions.signinFailure signinFailure}
+ * action creator.
+ *
+ * @type {String}
+ */
 export const SIGNIN_FAILURE = "SIGNIN_FAILURE";
+
+/**
+ * Fired by the {@link actions.signinRequest signinRequest}
+ * action creator.
+ *
+ * @type {String}
+ */
+export const SIGNIN_REQUEST = "SIGN_REQUEST";
+
+/**
+ * Fired by the {@link actions.signinSuccess signinSuccess}
+ * action creator.
+ *
+ * @type {String}
+ */
+export const SIGNIN_SUCCESS = "SIGNIN_SUCCESS";
