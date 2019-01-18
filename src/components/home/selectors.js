8c3fec828edcb4550734c16c18e7ea2cbcaba9a7
engreric <okemmadueric@gmail.com>
2019-01-18 23:13:29 +0100

    add comment to selector

--- src/components/home/selectors.js
+++ src/components/home/selectors.js
@@ -1,3 +1,11 @@
 import { NAME } from './constants';
 
+/**
+ * Selects the <tt>isSigninSuccessful</tt> key.
+ *
+ * @function
+ * @param {Object} state - redux store state
+ * @return {Boolean} the state of the home
+ * {@link module:home/constants::INITIAL_STATE constants::INITIAL_STATE}).
+ */
 export const getStatus = state => state[NAME].isMailSent;
