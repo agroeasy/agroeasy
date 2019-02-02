8be44377e8aebcde036075ca938f46e2c4134799
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-02-02 07:52:05 +0100

    actions

---
+++ src/components/app/actions.js
@@ -0,0 +1,21 @@
+import { SET_COOKIE, REMOVE_COOKIE } from './actionTypes';
+
+ /**
+ * Triggers request to set cookie
+ *
+ * @function
+ * @return {Object} The {@link actionTypes.SET_COOKIE SET_COOKIE} action.
+ */
+export const setCookie = () => ({
+    type: SET_COOKIE,
+});
+
+ /**
+ * Triggers request to remove cookie
+ *
+ * @function
+ * @return {Object} The {@link actionTypes.REMOVE_COOKIE REMOVE_COOKIE} action.
+ */
+export const removeCookie = () => ({
+    type: REMOVE_COOKIE,
+});
