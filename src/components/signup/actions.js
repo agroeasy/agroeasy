2b7a58f656ccc5b724d27a7d65f9c5e101d6c23d
engreric <okemmadueric@gmail.com>
2019-02-05 12:04:31 +0100

    [add] comments actions params

--- src/components/signup/actions.js
+++ src/components/signup/actions.js
@@ -4,6 +4,7 @@ import { SIGNUP_FAILURE, SIGNUP_REQUEST, SIGNUP_SUCCESS } from './actionTypes';
  * Triggers request to sign up the user
  *
  * @function
+ * @param {Object} [user] An object of user details captured from body
  * @return {Object} The {@link actionTypes.SIGNUP_REQUEST SIGNUP_REQUEST} action.
  */
 export const signupRequest = user => ({
@@ -15,6 +16,8 @@ export const signupRequest = user => ({
  * Triggers request to update redux store on success
  *
  * @function
+ * @param {Object} [data] An object containing signup status (success or failure) provided 
+ * the endpoint is reachable
  * @return {Object} The {@link actionTypes.SIGNUP_SUCCESS SIGNUP_SUCCESS} action.
  */
 export const signupSuccess = data => ({
@@ -24,8 +27,8 @@ export const signupSuccess = data => ({
 
 /**
  * Triggers request to update redux store on failure
- *
  * @function
+ * @param {Object} [error] An object containing error info if the signup endpoint is unreachable
  * @return {Object} The {@link actionTypes.SIGNUP_FAILURE SIGNUP_FAILURE} action.
  */
 export const signupFailure = error => ({
