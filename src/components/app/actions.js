ffbc54abdb3b413d717bf446b8c455e166d98885
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-02-09 16:07:24 +0100

    version bump

--- src/components/app/actions.js
+++ src/components/app/actions.js
@@ -4,7 +4,7 @@ import { SET_COOKIE, REMOVE_COOKIE } from './actionTypes';
  * Triggers request to set cookie
  *
  * @function
- * @params {String} token - a unique identifier used associated with the logged in user
+ * @params {String} token - a unique identifier used, associated with the logged in user
  * @return {Object} The {@link actionTypes.SET_COOKIE SET_COOKIE} action.
  */
 export const setCookie = token => ({
@@ -16,7 +16,7 @@ export const setCookie = token => ({
  * Triggers request to remove cookie
  *
  * @function
- * @params {String} token - a unique identifier used associated with the logged in user
+ * @params {String} token - a unique identifier used, associated with the logged in user
  * @return {Object} The {@link actionTypes.REMOVE_COOKIE REMOVE_COOKIE} action.
  */
 export const removeCookie = token => ({
