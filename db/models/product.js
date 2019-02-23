b6030a4001420afef04df8eeef7cc1cad9708d6c
engreric <okemmadueric@gmail.com>
2019-02-23 19:59:31 +0100

    [add] mongoosastic to sync es-search and mongodb

--- db/models/product.js
+++ db/models/product.js
@@ -1,15 +1,39 @@
 import mongoose from 'mongoose';
+import mongoosastic from 'mongoosastic';
+
+import esClient from '../../esClient';
 
 const Schema = mongoose.Schema;
 
-const ProductsSchema = new Schema({
+const ProductSchema = new Schema({
     createdAt: { type: Date },
     deletedAt: { type: Date },
     name: { required: true, type: String },
-    producerId: { required: true, type: String },
+    producerId: {  required: true, type: String },
+    productTypes: { required: true, type: String },
     quantity: { required: true, type: Number },
-    typeOfProducts: { required: true, type: String },
     updatedAt: { type: Date },
 }, { versionKey: false });
 
-export default mongoose.model('Product', ProductsSchema);
+ProductSchema.plugin(mongoosastic, {
+    esClient,
+    index: "all_product",
+    type: "products",
+});
+
+const Product = mongoose.model('Product', ProductSchema);
+
+/* const stream = Product.synchronize();
+let count = 0;
+
+stream.on('data', (err, doc) => {  
+    count++;
+});
+stream.on('close', () => {
+    console.log(`indexed ${count} documents!`);
+});
+stream.on('error', err => {
+    console.log(err);
+}); */
+
+export default Product;
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import signUp from './signUp';
 import producer from './producer';
 import product from './product';
 import contactUs from './contactUs';
+import search from './search';
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.use('/account', signUp);
 router.use('/account', contactUs);
 router.use('/producer', producer);
 router.use('/product', product);
+router.use('./api', search);
 
 export default router;
