1b9cd51103040c2d2ff096c974d24ed1493946f6
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-04-02 00:37:21 +0100

    minor fixes

--- src/components/app/reducers.js
+++ src/components/app/reducers.js
@@ -19,9 +19,9 @@ const initialState = {
 export default ( state = { ...initialState }, action) => {
     
     switch (action.type) {
-    case SET_COOKIE:    {
+    case SET_COOKIE: {
         const { data: { token, user }, status } = action.payload;
-        Cookies.set( token, user, { expires: EXPIRATION });
+        Cookies.set(token, user, { expires: EXPIRATION });
 
         return {
             ...state,
@@ -33,18 +33,18 @@ export default ( state = { ...initialState }, action) => {
     }
 
     case CHECK_USER_LOGIN_STATUS: {
-        const user = Cookies.get(state.token);        
+        const user = Cookies.get(state.token);
         
         return {
             ...state,
-            isLoggedIn: !!user === true,
+            isLoggedIn: !!user,
         };
     }
 
     case REMOVE_COOKIE: {
         Cookies.expire(state.token);
         
-        return { 
+        return {
             ...state,
             isLoggedIn: false,
             token: null,
@@ -64,4 +64,4 @@ export default ( state = { ...initialState }, action) => {
     default:
         return state;
     }
-}; 
+};
