45462f2fd3f6c74297fde581fd12131a89156d6a
Chinedu Ekene Okpala <Nedu63ima@gmail.com>
2019-05-04 13:38:00 +0000

    fix lint for db

--- db/models/producer.js
+++ db/models/producer.js
@@ -2,9 +2,12 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-const ProducersSchema = new Schema({
-    typeOfProducts: {  type: Array },
-    userId: { type: String },
-}, { versionKey: false });
+const ProducersSchema = new Schema(
+    {
+        typeOfProducts: { type: Array },
+        userId: { type: String }
+    },
+    { versionKey: false }
+);
 
 export default mongoose.model('Producer', ProducersSchema);
