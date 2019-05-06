338cf99da8f7c668034b0bc02a6b43c02fa94dad
Chinedu Ekene Okpala <Nedu63ima@gmail.com>
2019-05-06 09:47:50 +0000

    fix indent error

--- src/components/home/components/Carousel.js
+++ src/components/home/components/Carousel.js
@@ -4,20 +4,20 @@ import { CAROUSEL_DATA } from '../constants';
 
 //this is the carousel component
 class CarouselImages extends React.Component {
-  render() {
-    return (
-      <Carousel effect="fade" autoplay>
-        {CAROUSEL_DATA.map((data, index) => (
-          <div key={index}>
-            <img src={data.src} />
-            <div className="carousel-text">
-              <h3>{data.text}</h3>
-            </div>
-          </div>
-        ))}
-      </Carousel>
-    );
-  }
+    render() {
+        return (
+            <Carousel effect="fade" autoplay>
+                {CAROUSEL_DATA.map((data, index) => (
+                    <div key={index}>
+                        <img src={data.src} />
+                        <div className="carousel-text">
+                            <h3>{data.text}</h3>
+                        </div>
+                    </div>
+                ))}
+            </Carousel>
+        );
+    }
 }
 
 export default CarouselImages;
