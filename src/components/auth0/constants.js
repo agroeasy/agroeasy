4a24c790076eeed62b50ff56e7659ea62232ccee
engreric <okemmadueric@gmail.com>
2019-06-02 18:39:02 +0100

    [fix] lint error

--- src/components/auth0/constants.js
+++ src/components/auth0/constants.js
@@ -1,7 +1,7 @@
 export const SKELETON_DATA = {
     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    content:'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-    description: 'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:'We supply a series of design principles,and high quality design reso',
+    description: `Ant Design, a design language for background applications, is refined `,
     href: 'http://ant.design',
     title: 'ant design part',
 };
