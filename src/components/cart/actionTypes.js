22f3b258f23ddb9df40527b4d7b554b41bd770cd
Chinedu Ekene Okpala <Nedu63ima@gmail.com>
2019-07-01 11:52:41 +0000

    remove unneeded function

--- src/components/cart/actionTypes.js
+++
@@ -1,9 +0,0 @@
-import { NAME } from './constants';
-
-/**
- * Fired by the {@link actions.setUserData setUserData}
- * action creator.
- *
- * @type {String}
- */
-export const REMOVE_ITEM_FROM_CART = `${NAME}/REMOVE_ITEM_FROM_CART`;
