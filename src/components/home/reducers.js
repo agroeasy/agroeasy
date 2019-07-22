b80a553e2d27863922882b85449b7430a813182e
engreric <okemmadueric@gmail.com>
2019-07-22 12:35:52 +0100

    [fix] lint erros

--- src/components/home/reducers.js
+++ src/components/home/reducers.js
@@ -4,27 +4,25 @@ const initialState = {
     results: [],
 };
 
-export default ( state = { ...initialState },  action) => {
-
-    switch(action.type){
-    case REQUEST_SEARCH: {
-        const { payload } = action;
-        return {
-            ...state,
-            isloading: true,
-            payload,
-        };
+export default (state = { ...initialState }, action) => {
+    switch (action.type) {
+        case REQUEST_SEARCH: {
+            const { payload } = action;
+            return {
+                ...state,
+                isloading: true,
+                payload,
+            };
+        }
+        case UPDATE_SEARCH: {
+            const { payload } = action;
+            return {
+                ...state,
+                isloading: false,
+                results: [payload],
+            };
+        }
+        default:
+            return state;
     }
-    case UPDATE_SEARCH:{
-        const { payload } = action;
-        return{
-            ...state,
-            isloading: false,
-            results: [payload],
-        };
-    }
-    default:
-        return state;
-    }
-
 };
--- src/components/home/sagas.js
+++ src/components/home/sagas.js
@@ -1,24 +1,26 @@
 import { effects } from 'redux-saga';
-import {  REQUEST_SEARCH } from './actionTypes';
+import { REQUEST_SEARCH } from './actionTypes';
 import { SEARCH_URL } from './constants';
 import { updateSearchResult } from './actions';
 
 function* searchProduct(action) {
     try {
-        const { payload:{ searchTerm, value } } = action;
+        const {
+            payload: { searchTerm, value },
+        } = action;
 
         const response = yield fetch(`${SEARCH_URL}?${searchTerm}=${value}`, {
             method: 'GET',
         });
 
-        if(response.ok){
+        if (response.ok) {
             const data = yield response.json();
             yield effects.put(updateSearchResult(data));
         }
     } catch (error) {
         // eslint-disable-next-line no-console
         console.log(error);
-    } 
+    }
 }
 
 function* watchSearchProduct() {
@@ -30,8 +32,6 @@ function* watchSearchProduct() {
     }
 }
 
-export default function* () {
-    yield effects.all([
-        watchSearchProduct(),
-    ]);
+export default function*() {
+    yield effects.all([watchSearchProduct()]);
 }
